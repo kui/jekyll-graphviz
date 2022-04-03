d448751547e04a1ca495c262c3c9bd3a37cead6e
Zhigang Song <song@iuwei.fun>
2022-04-03 18:28:12 +0800

    feat: include dot file support

--- lib/jekyll/graphviz.rb
+++ lib/jekyll/graphviz.rb
@@ -1,6 +1,7 @@
 require "jekyll"
 require "jekyll/graphviz/version"
 require "jekyll/graphviz/block"
+require "jekyll/graphviz/dotf"
 
 module Jekyll
   module Graphviz
@@ -10,3 +11,4 @@ end
 Liquid::Template.register_tag('graphviz', Jekyll::Graphviz::Block)
 Liquid::Template.register_tag('graph', Jekyll::Graphviz::Block)
 Liquid::Template.register_tag('digraph', Jekyll::Graphviz::Block)
+Liquid::Template.register_tag('dotf', Jekyll::Graphviz::DotF)
